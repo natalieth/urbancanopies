91d9abf8d85a6702d705eca644d495b453265c68
Natalie Theeuwes <wg914728@cluster-login-0-1.local>
2018-11-08 17:52:37 +0000

    second, still testing

--- read_dns.py
+++ read_dns.py
@@ -1,11 +1,11 @@
-import netCDF4 as nc
+from scipy.io import loadmat
 
 class Zerocubes: 
     dir = '/export/cloud/NCASweather/nx902220/DNS_LES/DNS_data/0degcubesFlowData/'
     file = 'r16168SENS.mat'
 
-def readvar(self,var): 
-    F = nc.Dataset(self)
-    self.var = F.variables[var][:]
+def readvar(fi,var): 
+    F = loadmat(fi)
+    self.var = F[var][:]
     return self.var
 
